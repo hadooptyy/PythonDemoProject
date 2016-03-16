52f01e1054dd83c13e93344aea2666b3209eade5
hadooptyy <hadooptyy@163.com>
2016-03-16 17:26:37 +0800

    first commit

---
+++ demo01.py
@@ -0,0 +1,19 @@
+# coding=utf-8
+
+"这是第一个Python脚本"
+
+# 这是单行注释
+new_str = "这是一个全局变量"
+
+# 这是函数
+def hello():
+    """
+    这是多行注释
+    多行注释内容
+    @author Wells
+    """
+    return "hello world!"
+
+# 这是程序主体
+if __name__ == "__main__":
+    print(hello())
---
+++ demo02.py
@@ -0,0 +1,7 @@
+# coding=utf-8
+
+import demo01
+
+print(demo01.__doc__)
+print(demo01.new_str)
+print(demo01.hello())
